c6dc955d100b18d8b94a97e8623cb9d0872c067f
nathgunajit <gunajitnath10@gmail.com>
2018-02-11 15:52:10 +0530

    Revert "InitializationExample1 add"

--- src/main/java/com/java/collections/arraylist/InitializationExample1.java
+++
@@ -1,9 +0,0 @@
-package com.java.collections.arraylist;
-import java.util.*;
-public class InitializationExample1 {
-   public static void main(String args[]) {
-	   ArrayList<String> obj = new ArrayList<String>(
-		Arrays.asList("Pratap", "Peter", "Harsh"));
-	  System.out.println("Elements are:"+obj);
-   }
-}
