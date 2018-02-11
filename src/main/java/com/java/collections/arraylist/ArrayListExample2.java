23824021e423498c8f9d7da6af6d03e23507044c
nathgunajit <gunajitnath10@gmail.com>
2018-02-11 22:59:39 +0530

    basics sorting wrapper

---
+++ src/main/java/com/java/collections/arraylist/ArrayListExample2.java
@@ -0,0 +1,12 @@
+package com.java.collections.arraylist;
+import java.util.*;
+public class ArrayListExample2 {
+   public static void main(String args[]) {
+	   ArrayList<String> cities = new ArrayList<String>(){{
+		   add("Delhi");
+		   add("Agra");
+		   add("Chennai");
+		   }};
+	  System.out.println("Content of Array list cities:"+cities);
+   }
+}
